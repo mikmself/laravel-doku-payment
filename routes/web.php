b89c7dce98715bccaa734c3bf13bdb60c7d1efcf
mikmself <mikmself@gmail.com>
2024-12-10 13:33:12 +0700

    feat : add route to login

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('index');
 Route::get('products', [\App\Http\Controllers\HomeController::class, 'products'])->name('products');
+
+Route::get('login', [\App\Http\Controllers\AuthController::class, 'loginPage'])->name('login-page');
