4db31dd0c5b7b6f195d02c9e4ea629cc9598c5b4
mikmself <mikmself@gmail.com>
2024-12-10 13:15:40 +0700

    feat : add route products

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
         return view('index');
+    }
+    public function products(){
+        $products = Product::all();
+        return view('products', compact('products'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('index');
+Route::get('products', [\App\Http\Controllers\HomeController::class, 'products'])->name('products');
